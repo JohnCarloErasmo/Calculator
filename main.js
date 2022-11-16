9cc0edac7b17f8348505399d09c2890ed4ff0be7
unknown <JohnCarloErasmo>
2022-11-16 15:38:38 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function display(vaL)
+{
+    document.getElementById('result').value += vaL
+    return vaL
+}
+
+function solve()
+{
+    let x = document.getElementById('result').value
+    let y = eval(x);
+    document.getElementById('result').value = y 
+    return y
+}
+
+function clearScreen()
+{
+    document.getElementById('result').value = "";
+}
